348afcd5c671ec478cfa74a727f6d8b0699b0191
lxlxiaolin <xlluo@thoughtworks.com>
2015-10-01 10:31:29 +0800

    show message and show book details

--- Biblioteca/src/com/Biblioteca/Biblioteca.java
+++ Biblioteca/src/com/Biblioteca/Biblioteca.java
@@ -8,6 +8,7 @@ import java.util.List;
  */
 public class Biblioteca {
     public static List<Book> bookList = new ArrayList<>();
+
     public static String welcomeMessage(){
         return "welcome";
     }
--- Biblioteca/src/com/Biblioteca/Main.java
+++ Biblioteca/src/com/Biblioteca/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Biblioteca.welcomeMessage();
+
+        Biblioteca.bookList();
+
+        Book book = new Book();
+        book.Name = "a";
+        book.ISBN = "123";
+        Biblioteca.selectedBook(book);
     }
 }
